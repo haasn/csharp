78c7d58a65a79854019ebd15e1d609018858e94b
nand <nand@lavabit.com>
2012-10-02 20:56:47 +0200

    Add CoRecursion example
    Ignore-this: 59f1632581763c6e781dd020e39f947c
    
    darcs-hash:20121002185647-cd4f6-b0ca76018e3531a7ebe7855ff4f349a29ad10c7e

---
+++ CoRecursion/CoRecursion.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+// Mutable indirection node
+class IVar<A> { public A it; }
+
+// Recursive type
+class Rec : Mul<bool, IVar<Rec>> {
+  public Rec (bool pi1, IVar<Rec> pi2) : base (pi1, pi2) {}}
+
+static partial class Program {
+  static Queue<Action> ctors;
+
+  static object Ctor (Action a) {
+    if (ctors == null)
+      ctors = new Queue<Action> ();
+
+    ctors.Enqueue (a);
+    return null; }
+
+  static Program () {
+    foreach (var a in ctors) a(); }
+
+  static void PrintRec (Rec r) {
+    Console.WriteLine (r.π1);
+    PrintRec (r.π2.it); }
+
+  static void Main () {
+    PrintRec (a.it); }}
+
+// Actual variables
+
+static partial class Program {
+  public static IVar<Rec> a = new IVar<Rec> ();
+
+  static object ctor_a = Ctor (delegate {
+    a.it = new Rec (true, b); }); }
+
+static partial class Program {
+  public static IVar<Rec> b = new IVar<Rec> ();
+
+  static object ctor_b = Ctor (delegate {
+    b.it = new Rec (false, a); }); }
